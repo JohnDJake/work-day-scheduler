cb4164cd79afec040c5da20149f55245bc8e91f7
JohnDJake <johndjake@gmail.com>
2020-12-16 14:19:16 -0800

    added code to save and load entered schedule events

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,31 @@
 const DateTime = luxon.DateTime;
 
-$(document).ready(function() {
+$(document).ready(function () {
+    // get the current date and time
     var currentDateTime = DateTime.local();
+    // get the stored events
+    var storedEvents = JSON.parse(localStorage.getItem("dailySchedule")) || {};
+    // display the date on the page
     $("#currentDay").text(currentDateTime.toLocaleString(DateTime.DATE_HUGE));
-    $(".description").each(function(index, el) {
-        var elTime = parseInt($(el).attr("data-time"));
-        if (elTime < currentDateTime.hour) {
+    // set the text for each textarea from the stored data
+    // and set the formatting for each textarea based off the time
+    $(".description").each(function (index, el) {
+        // keys in stored events match the textarea ID
+        $(el).val(storedEvents[$(el).attr("id")]);
+        var time = parseInt($(el).attr("data-time"));
+        if (time < currentDateTime.hour) {
             $(el).addClass("past");
-        } else if (elTime == currentDateTime.hour) {
+        } else if (time == currentDateTime.hour) {
             $(el).addClass("present");
         } else {
             $(el).addClass("future");
         }
     });
+
+    // update the stored value for only the corresponding hour when a save button is pressed
+    $(".saveBtn").click(function (event) {
+        event.preventDefault();
+        storedEvents[$(event.target).attr("data-ref")] = $("#" + $(event.target).attr("data-ref")).val();
+        localStorage.setItem("dailySchedule", JSON.stringify(storedEvents));
+    })
 })
